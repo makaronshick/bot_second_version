529d8c37fa1c15918bf8d92ea4597d462dc97d44
Andrii <makaronshick@ukr.net>
2021-12-17 22:50:33 +0200

    Project added

---
+++ public/index.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once('../vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(realpath(__DIR__ . '/..'));
+$dotenv->load();
+
+$token = $_ENV['BOT_TOKEN'];
+
+$url = 'https://api.telegram.org/bot' . $token . '/getUpdates';
+$sendMessageUrl = 'https://api.telegram.org/bot' . $token . '/sendMessage';
+$lastUpdateId = null;
+$_url = $url;
+
+do {
+    if (null !== $lastUpdateId) {
+        $_url = $url . '?offset=' . $lastUpdateId;
+    }
+
+    $response = json_decode(file_get_contents($_url), true);
+
+    $text = json_decode(file_get_contents('https://icanhazdadjoke.com/slack'), true);;
+
+    foreach ($response['result'] as $result) {
+        if ($lastUpdateId == $result['update_id']) {
+            continue;
+        }
+        $lastUpdateId = $result['update_id'];
+
+        $chatId = $result['message']['chat']['id'];
+        $messageId = $result['message']['message_id'];
+
+        $response = [
+            'chat_id' => $chatId,
+            'text' => $text["attachments"][0]["text"],
+        ];
+
+        file_get_contents($sendMessageUrl . '?' . http_build_query($response));
+    }
+    sleep(5);
+} while (true);
